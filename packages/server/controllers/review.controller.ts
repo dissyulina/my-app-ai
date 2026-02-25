77a57787894de7a97e33313b678e3440c2349205
Dissy Ulina <dissy.ulina@ict.nl>
2026-02-25 15:17:38 +0100

    Build the backend for summarizing reviews

--- packages/server/controllers/review.controller.ts
+++ packages/server/controllers/review.controller.ts
@@ -6,12 +6,23 @@ export const reviewController = {
     const productId = Number(req.params.id);
 
     if (isNaN(productId)) {
-      res.status(400).json({ error: 'Invalud product ID.' });
+      res.status(400).json({ error: 'Invalid product ID.' });
       return;
     }
 
     const reviews = await reviewService.getReviews(productId);
-
     res.json(reviews);
+  },
+
+  async summarizeReviews(req: Request, res: Response) {
+    const productId = Number(req.params.id);
+
+    if (isNaN(productId)) {
+      res.status(400).json({ error: 'Invalid product ID.' });
+      return;
+    }
+
+    const summary = await reviewService.summarizeReviews(productId);
+    res.json({ summary });
   },
 };
--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -2,7 +2,7 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient, type Review } from '../generated/prisma/client';
 
 export const reviewRepository = {
-  async getReviews(productId: number): Promise<Review[]> {
+  async getReviews(productId: number, limit?: number): Promise<Review[]> {
     const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
     const prisma = new PrismaClient({ adapter });
 
@@ -10,6 +10,7 @@ export const reviewRepository = {
     return prisma.review.findMany({
       where: { productId },
       orderBy: { createdAt: 'desc' },
+      take: limit,
     });
   },
 };
