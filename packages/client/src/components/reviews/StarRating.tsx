0152b7d1587139ce7b5531cfdca2e73eff872e7c
Dissy Ulina <dissy.ulina@ict.nl>
2026-03-02 16:36:56 +0100

    Display the reviews and the star rating on the page

---
+++ packages/client/src/components/reviews/StarRating.tsx
@@ -0,0 +1,18 @@
+import { FaRegStar, FaStar } from 'react-icons/fa';
+
+type StarRatingProps = {
+  value: number;
+};
+const StarRating = ({ value }: StarRatingProps) => {
+  const placeholders = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="flex gap-1 text-yellow-500">
+      {placeholders.map((p) =>
+        p <= value ? <FaStar key={p} /> : <FaRegStar key={p} />
+      )}
+    </div>
+  );
+};
+
+export default StarRating;
