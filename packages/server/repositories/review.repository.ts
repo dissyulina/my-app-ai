bbc824682b2a26317e5c92f6da9ca497e92d7931
Dissy Ulina <dissy.ulina@ict.nl>
2026-03-02 13:50:47 +0100

    Add regeneration and edge cases handling

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -1,16 +1,47 @@
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient, type Review } from '../generated/prisma/client';
+import dayjs from 'dayjs';
+
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+const prisma = new PrismaClient({ adapter });
 
 export const reviewRepository = {
   async getReviews(productId: number, limit?: number): Promise<Review[]> {
-    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
-    const prisma = new PrismaClient({ adapter });
-
     // SELECT * FROM reviews WHERE productId = @productId ORDER BY createdAt DESC
     return prisma.review.findMany({
       where: { productId },
       orderBy: { createdAt: 'desc' },
       take: limit,
     });
+  },
+
+  storeReviewSummary(productId: number, summary: string) {
+    const now = new Date();
+    const expiresAt = dayjs().add(7, 'days').toDate();
+    const data = {
+      content: summary,
+      expiresAt,
+      generatedAt: now,
+      productId,
+    };
+
+    return prisma.summary.upsert({
+      where: { productId },
+      create: data,
+      update: data,
+    });
+  },
+
+  async getReviewSummary(productId: number): Promise<string | null> {
+    const summary = await prisma.summary.findFirst({
+      where: {
+        AND: [
+          { productId },
+          { expiresAt: { gt: new Date() } }, // WHERE expiresAt > ...
+        ],
+      },
+    });
+
+    return summary ? summary.content : null;
   },
 };
